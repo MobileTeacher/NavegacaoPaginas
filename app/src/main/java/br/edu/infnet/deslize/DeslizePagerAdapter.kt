5afe7fa1a4b7f006d0437018110a239185a4b4ad
Hallison Paz <hallison.dapaz@gmail.com>
2020-01-26 13:26:44 -0300

    Tablayout working with ViewPager

--- app/src/main/java/br/edu/infnet/deslize/DeslizePagerAdapter.kt
+++ app/src/main/java/br/edu/infnet/deslize/DeslizePagerAdapter.kt
@@ -8,6 +8,8 @@ class DeslizePagerAdapter(fm: FragmentManager):
     FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     val numeroDeFragmentos = 2
+    // lista de títulos para as abas
+    val titulos = listOf("Texto", "Imagem")
 
     // retorna o fragmento adequado ao índice
     override fun getItem(position: Int) = when(position){
@@ -17,4 +19,8 @@ class DeslizePagerAdapter(fm: FragmentManager):
 
     // retorna o total de fragmentos
     override fun getCount() = numeroDeFragmentos
+
+    // retorna o título adequado à aba do índice
+    override fun getPageTitle(position: Int)
+            = titulos[position]
 }
--- app/src/main/java/br/edu/infnet/deslize/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/deslize/MainActivity.kt
@@ -10,11 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpViewPager()
+    }
+
+    private fun setUpViewPager(){
         // Em uma activity se passa supportFragmentManager
         // Se o ViewPager estiver dentro de um fragmento
         // você deve passar childFragmentManager para
         // exibir fragmentos dentro de um fragmento
         viewpager.adapter =
             DeslizePagerAdapter(supportFragmentManager)
+        // TabLayout em conjunto com ViewPager
+        tablayout.setupWithViewPager(viewpager)
     }
 }
